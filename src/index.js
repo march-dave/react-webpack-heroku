4c68c49b8bca200301102381c5ccd34e86bb7a39
march-dave <ysangy@gmail.com>
2017-03-06 20:46:10 -0500

    reducer and router create

--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
 import React from 'react'
-import { render } from 'react-dom'
+import ReactDOM from 'react-dom'
+import { Router, Route, browserHistory, IndexRoute } from 'react-router'
 import App from './components/app'
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reducers from 'reducers';
+import thunk from 'redux-thunk';
+
 import './styles/app.scss'
 
-render(<App/>, document.getElementById('main'))
+
+
+
+const store = createStore(reducers, applyMiddleware(thunk));
+
+const rootElement = document.getElementById('main');
+ReactDOM.render(
+    <Provider store={store}>
+        <Router history={browserHistory}>
+            <Route path="/" component={App}>
+                <IndexRoute component={Home}/>
+                <Route path="home" component={Home}/>
+                <Route path="login" component={Login}/>
+                <Route path="register" component={Register}/>
+                <Route path="wall/:username" component={Wall}/>
+            </Route>
+        </Router>
+    </Provider>, rootElement
+);
+
