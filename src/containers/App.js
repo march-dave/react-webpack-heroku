8e25f7a1ceabf547897c2cc26bcbca23657d3266
march-dave <ysangy@gmail.com>
2017-03-09 20:57:27 -0500

    reduce action

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 
 class App extends Component {
     render() {
-        return (<div>
+        return (
+            <div>
 
             </div>
         );
