44549453e5f69eaae5092d15acd9937cfa7baaf9
march-dave <ysangy@gmail.com>
2017-03-26 16:18:23 -0400

    reducers index testing

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
-// import authentication from './authentication';
-// import memo from './memo';
-// import search from './search';
+import authentication from './authentication';
+import memo from './memo';
+import search from './search';
 
 
-// import { combineReducers } from 'redux';
+import { combineReducers } from 'redux';
 
-// export default combineReducers({
-//     authentication, memo, search
-// });
+export default combineReducers({
+    authentication,
+    memo,
+    search
+});
