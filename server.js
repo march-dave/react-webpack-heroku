d2089b7bdea09716f3e1e29cea487acf5a179f07
march-dave <ysangy@gmail.com>
2017-03-30 21:04:21 -0400

    graphql with express start

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const express = require('express');
+const graphqlHTTP = require('express-graphql');
 
 const { graphql, buildSchema } = require('graphql');
 
@@ -14,6 +15,14 @@ var root = { hello: () => 'Hello World!' };
 graph(schema, '{Hello}', root).then((response) => {
     console.log(response);
 });
+
+const app = express();
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphql: true,
+}));
+app.listen(4000, () => console.log('Now browse to localhost:4000/graphql'));
 
 module.exports = {
     app: function() {
