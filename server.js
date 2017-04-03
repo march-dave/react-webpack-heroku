3fdd6af3bc82fa7f500837b014ec0699c7178f15
march-dave <ysangy@gmail.com>
2017-04-03 04:54:42 -0400

    up to date

--- server.js
+++ server.js
@@ -81,3 +81,9 @@ app.use('/graphql', graphqlHTTP({
     graphiql: true,
 }));
 app.listen(4000, () => console.log('Now browse to localhost:4000/graphql'));
+
+var schema = makeExecutableSchema({ typeDefs, resolvers });
+var app = express();
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+app.listen(4000, () => console.log('Now browse to localhost:4000/graphiql'));
