444b3827dd037721791971982b48da521745a16f
march-dave <ysangy@gmail.com>
2017-04-16 12:39:26 -0400

    up to date

--- server.js
+++ server.js
@@ -349,3 +349,17 @@ import { Text, StyleSheet, Linking, ScrollView, ActivityIndicator, RefreshContro
 import { Constants } from 'expo';
 import { gql, ApolloClient, createNetworkInterface, ApolloProvider, graphql } from 'react-apollo';
 import { List, ListItem, Button } from 'react-native-elements';
+
+var { graphql, buildSchema } = require('graphql');
+
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+var root = { hello: () => 'Hello world!' };
+
+graphql(schema, '{ hello }', root).then((response) => {
+    console.log(response);
+});
