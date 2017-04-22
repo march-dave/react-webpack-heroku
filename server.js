048726d44ffad332e2608f494dcb467bdd3df5bb
march-dave <ysangy@gmail.com>
2017-04-21 20:42:36 -0400

    graphql server

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-var { graphql, buildSchema } = require('graphql');
+var express = require('express');
+var graphqlHttp = require('express-graphql');
+var { buildSchema } = require('graphql');
 
 var schema = buildSchema(`
   type Query {
@@ -6,8 +8,13 @@ var schema = buildSchema(`
   }
 `);
 
-var root = { hello: () => 'Hello world!' };
+var root = { hello: () => 'hello world!' };
 
-graphql(schema, '{ hello }', root).then((response) => {
-    console.log(response);
-});
+var app = express();
+app.use('graphql', graphqlHttp({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
+
+app.listen(4000, () => console.log('Now browse to localhost:4000/graphql'));
