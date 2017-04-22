ffce02aba3d34269359d8d2298e50c3391589298
march-dave <ysangy@gmail.com>
2017-04-22 15:09:22 -0400

    graphql server continue

--- server.js
+++ server.js
@@ -18,3 +18,32 @@ app.use('graphql', graphqlHttp({
 }));
 
 app.listen(4000, () => console.log('Now browse to localhost:4000/graphql'));
+
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var { graphqlExpress, graphiqlExpress } = require('graphql-server-express');
+var { makeExecutableSchema } = require('graphql-tools');
+
+var typeDefs = [`
+type Query {
+  hello: String
+}
+
+schema {
+  query: Query
+}`];
+
+var resolvers = {
+    Query: {
+        hello(root) {
+            return 'world';
+        }
+    }
+};
+
+var schema = makeExecutableSchema({ typeDefs, resolvers });
+var app = express();
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+app.listen(4000, () => console.log('Now browse to localhost:4000/graphiql'));
