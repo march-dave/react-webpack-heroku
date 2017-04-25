27a33078d930c2c26d5cc1040db0654218112a44
march-dave <ysangy@gmail.com>
2017-04-24 20:50:54 -0400

    test code

--- server.js
+++ server.js
@@ -64,3 +64,15 @@ app.listen(4000, () => console.log('Now browse to localhost:4000/graphiql'));
 // end
 
 // puts Schema.execute('{ hello }')
+
+// import graphene
+
+// class Query(graphene.ObjectType):
+//   hello = graphene.String()
+
+//   def resolve_hello(self, args, context, info):
+//     return 'Hello world!'
+
+// schema = graphene.Schema(query=Query)
+// result = schema.execute('{ hello }')
+// print(result.data['hello'])
