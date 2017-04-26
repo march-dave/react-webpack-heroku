c21b6994e3c2f4ee8d3aefe01dbfb695a55c6760
march-dave <ysangy@gmail.com>
2017-04-26 04:24:08 -0400

    code comment

--- server.js
+++ server.js
@@ -1,83 +1,6 @@
-var express = require('express');
-var graphqlHttp = require('express-graphql');
-var { buildSchema } = require('graphql');
-
-var schema = buildSchema(`
-  type Query {
-    hello: String
-  }
-`);
-
-var root = { hello: () => 'hello world!' };
-
-var app = express();
-app.use('graphql', graphqlHttp({
-    schema: schema,
-    rootValue: root,
-    graphiql: true,
-}));
-
-app.listen(4000, () => console.log('Now browse to localhost:4000/graphql'));
-
-
-var express = require('express');
-var bodyParser = require('body-parser');
-var { graphqlExpress, graphiqlExpress } = require('graphql-server-express');
-var { makeExecutableSchema } = require('graphql-tools');
-
-var typeDefs = [`
-type Query {
-  hello: String
-}
-
-schema {
-  query: Query
-}`];
-
-var resolvers = {
-    Query: {
-        hello(root) {
-            return 'world';
-        }
-    }
-};
-
-var schema = makeExecutableSchema({ typeDefs, resolvers });
-var app = express();
-app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
-app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
-app.listen(4000, () => console.log('Now browse to localhost:4000/graphiql'));
-
-
-// require 'graphql'
-
-// QueryType = GraphQL::ObjectType.define do
-//   name 'Query'
-//   field :hello do
-//     type types.String
-//     resolve -> (obj, args, ctx) { 'Hello world!' }
-//   end
-// end
-
-// Schema = GraphQL::Schema.define do
-//   query QueryType
-// end
-
-// puts Schema.execute('{ hello }')
-
-// import graphene
-
-// class Query(graphene.ObjectType):
-//   hello = graphene.String()
-
-//   def resolve_hello(self, args, context, info):
-//     return 'Hello world!'
-
-// schema = graphene.Schema(query=Query)
-// result = schema.execute('{ hello }')
-// print(result.data['hello'])
-
-// var { graphql, buildSchema } = require('graphql');
+// var express = require('express');
+// var graphqlHttp = require('express-graphql');
+// var { buildSchema } = require('graphql');
 
 // var schema = buildSchema(`
 //   type Query {
@@ -85,8 +8,85 @@ app.listen(4000, () => console.log('Now browse to localhost:4000/graphiql'));
 //   }
 // `);
 
-// var root = { hello: () => 'Hello world!' };
+// var root = { hello: () => 'hello world!' };
 
-// graphql(schema, '{ hello }', root).then((response) => {
-//     console.log(response);
-// });
+// var app = express();
+// app.use('graphql', graphqlHttp({
+//     schema: schema,
+//     rootValue: root,
+//     graphiql: true,
+// }));
+
+// app.listen(4000, () => console.log('Now browse to localhost:4000/graphql'));
+
+
+// var express = require('express');
+// var bodyParser = require('body-parser');
+// var { graphqlExpress, graphiqlExpress } = require('graphql-server-express');
+// var { makeExecutableSchema } = require('graphql-tools');
+
+// var typeDefs = [`
+// type Query {
+//   hello: String
+// }
+
+// schema {
+//   query: Query
+// }`];
+
+// var resolvers = {
+//     Query: {
+//         hello(root) {
+//             return 'world';
+//         }
+//     }
+// };
+
+// var schema = makeExecutableSchema({ typeDefs, resolvers });
+// var app = express();
+// app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+// app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+// app.listen(4000, () => console.log('Now browse to localhost:4000/graphiql'));
+
+
+// // require 'graphql'
+
+// // QueryType = GraphQL::ObjectType.define do
+// //   name 'Query'
+// //   field :hello do
+// //     type types.String
+// //     resolve -> (obj, args, ctx) { 'Hello world!' }
+// //   end
+// // end
+
+// // Schema = GraphQL::Schema.define do
+// //   query QueryType
+// // end
+
+// // puts Schema.execute('{ hello }')
+
+// // import graphene
+
+// // class Query(graphene.ObjectType):
+// //   hello = graphene.String()
+
+// //   def resolve_hello(self, args, context, info):
+// //     return 'Hello world!'
+
+// // schema = graphene.Schema(query=Query)
+// // result = schema.execute('{ hello }')
+// // print(result.data['hello'])
+
+// // var { graphql, buildSchema } = require('graphql');
+
+// // var schema = buildSchema(`
+// //   type Query {
+// //     hello: String
+// //   }
+// // `);
+
+// // var root = { hello: () => 'Hello world!' };
+
+// // graphql(schema, '{ hello }', root).then((response) => {
+// //     console.log(response);
+// // });
