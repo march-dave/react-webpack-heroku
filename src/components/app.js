15b5ca6e2c3966c866c99831b30aa8a57eee0f42
march-dave <ysangy@gmail.com>
2017-07-16 00:22:02 -0400

    Add turnary

--- src/components/app.js
+++ src/components/app.js
@@ -2,12 +2,15 @@ import React from 'react'
 
 export default class App extends React.Component {
   render () {
+    var i = 1;
     return (
       <div>
           <h1>Header</h1>
           <h2>Content</h2>
           <p data-myattribue="somevalue">This is the content!!</p>
           <h1>{1+1}</h1>
+
+          <h1>{ i == 1 ? 'True': 'False'}</h1>
       </div>
     )
   }
