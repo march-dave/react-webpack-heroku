d58cd491a81ea3bec82450ee18275fda26dfc4f9
march-dave <ysangy@gmail.com>
2017-07-16 07:54:04 -0400

    style apply

--- src/components/app.js
+++ src/components/app.js
@@ -2,10 +2,16 @@ import React from 'react'
 
 export default class App extends React.Component {
   render () {
+
+    var myStyle = {
+      fontSize: 100,
+      color: '#00FF00'
+    }
+
     var i = 1;
     return (
       <div>
-          <h1>Header</h1>
+          <h1 style={myStyle}>Header</h1>
           <h2>Content</h2>
           <p data-myattribue="somevalue">This is the content!!</p>
           <h1>{1+1}</h1>
