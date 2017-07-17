a5c1110b8aafdcde289864dc3da1013acfd1131d
march-dave <ysangy@gmail.com>
2017-07-17 04:02:58 -0400

    up to date

--- src/components/app.js
+++ src/components/app.js
@@ -13,8 +13,8 @@ export default class App extends React.Component {
       <div>
           <h1 style={myStyle}>Header</h1>
           <h2>Content</h2>
-          <p>This is the content!!</p>
-          <h1>{1+1}</h1>
+          {/* <p>This is the content!!</p>
+          <h1>{1+1}</h1> */}
 
           <h1>{ i == 1 ? 'True': 'False'}</h1>
       </div>
