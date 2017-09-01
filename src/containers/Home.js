81977916b258c550a83bb0a8ca90946db1a95657
march-dave <ysangy@gmail.com>
2017-09-01 03:58:37 -0400

    props

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -4,7 +4,7 @@ class Home extends Component {
     render() {
         return (
             <div>
-                Home Page
+                {/* {this.props.} */}
             </div>
         );
     }
