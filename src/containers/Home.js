0bb2b499c42dd9a658680873b4254708d6466e90
march-dave <ysangy@gmail.com>
2017-09-03 18:36:10 -0400

    Home pag error commit out

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -4,7 +4,7 @@ class Home extends Component {
     render() {
         return (
             <div>
-                {this.props.}
+                {/* {this.props.} */}
             </div>
         );
     }
