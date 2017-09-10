d6a63e805ec0ec014db519c146ae512369081445
Dave Lee <ysangy@gmail.com>
2017-09-09 21:14:43 -0400

    Port number switch

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const Server = require('./server.js')
-const PORT = (process.env.PORT || 3030)
+const PORT = (process.env.PORT || 3000)
 const app = Server.app()
 
 if (process.env.NODE_ENV !== 'production') {
