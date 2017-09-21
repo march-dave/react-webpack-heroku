479a5c9cca2fc926c89068fe9b4f9e106bf6d095
march-dave <ysangy@gmail.com>
2017-09-20 20:51:42 -0400

    start page error

--- server.js
+++ server.js
@@ -1,99 +1,83 @@
-// var express = require('express');
-// var graphqlHttp = require('express-graphql');
-// var { buildSchema } = require('graphql');
+var express = require('express');
+var graphqlHttp = require('express-graphql');
+var { buildSchema } = require('graphql');
 
-// var schema = buildSchema(`
-//   type Query {
-//     hello: String
-//   }
-// `);
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
 
-// var root = { hello: () => 'hello world!' };
+var root = { hello: () => 'hello world!' };
 
-// var app = express();
-// app.use('graphql', graphqlHttp({
-//     schema: schema,
-//     rootValue: root,
-//     graphiql: true,
-// }));
+var app = express();
+app.use('graphql', graphqlHttp({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
 
-// app.listen(4000, () => console.log('Now browse to localhost:4000/graphql'));
-
-
-// var express = require('express');
-// var bodyParser = require('body-parser');
-// var { graphqlExpress, graphiqlExpress } = require('graphql-server-express');
-// var { makeExecutableSchema } = require('graphql-tools');
-
-// var typeDefs = [`
-// type Query {
-//   hello: String
-// }
-
-// schema {
-//   query: Query
-// }`];
-
-// var resolvers = {
-//     Query: {
-//         hello(root) {
-//             return 'world';
-//         }
-//     }
-// };
-
-// var schema = makeExecutableSchema({ typeDefs, resolvers });
-// var app = express();
-// app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
-// app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
-// app.listen(4000, () => console.log('Now browse to localhost:4000/graphiql'));
+app.listen(4000, () => console.log('Now browse to localhost:4000/graphql'));
 
 
-// // require 'graphql'
+var express = require('express');
+var bodyParser = require('body-parser');
+var { graphqlExpress, graphiqlExpress } = require('graphql-server-express');
+var { makeExecutableSchema } = require('graphql-tools');
 
-// // QueryType = GraphQL::ObjectType.define do
-// //   name 'Query'
-// //   field :hello do
-// //     type types.String
-// //     resolve -> (obj, args, ctx) { 'Hello world!' }
-// //   end
-// // end
+var typeDefs = [`
+type Query {
+  hello: String
+}
 
-// // Schema = GraphQL::Schema.define do
-// //   query QueryType
-// // end
+schema {
+  query: Query
+}`];
 
-// // puts Schema.execute('{ hello }')
+var resolvers = {
+    Query: {
+        hello(root) {
+            return 'world';
+        }
+    }
+};
 
-// // import graphene
+var schema = makeExecutableSchema({ typeDefs, resolvers });
+var app = express();
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+app.listen(4000, () => console.log('Now browse to localhost:4000/graphiql'));
 
-// // class Query(graphene.ObjectType):
-// //   hello = graphene.String()
 
-// //   def resolve_hello(self, args, context, info):
-// //     return 'Hello world!'
+// require 'graphql'
 
-// // schema = graphene.Schema(query=Query)
-// // result = schema.execute('{ hello }')
-// // print(result.data['hello'])
+// QueryType = GraphQL::ObjectType.define do
+//   name 'Query'
+//   field :hello do
+//     type types.String
+//     resolve -> (obj, args, ctx) { 'Hello world!' }
+//   end
+// end
 
-// // var { graphql, buildSchema } = require('graphql');
+// Schema = GraphQL::Schema.define do
+//   query QueryType
+// end
 
-// // var schema = buildSchema(`
-// //   type Query {
-// //     hello: String
-// //   }
-// // `);
+// puts Schema.execute('{ hello }')
 
-// // var root = { hello: () => 'Hello world!' };
+// import graphene
 
-// // graphql(schema, '{ hello }', root).then((response) => {
-// //     console.log(response);
-// // });
+// class Query(graphene.ObjectType):
+//   hello = graphene.String()
 
-// var express = require('express');
-// var graphqlHTTP = require('express-graphql');
-// var { buildSchema } = require('graphql');
+//   def resolve_hello(self, args, context, info):
+//     return 'Hello world!'
+
+// schema = graphene.Schema(query=Query)
+// result = schema.execute('{ hello }')
+// print(result.data['hello'])
+
+// var { graphql, buildSchema } = require('graphql');
 
 // var schema = buildSchema(`
 //   type Query {
@@ -103,38 +87,54 @@
 
 // var root = { hello: () => 'Hello world!' };
 
-// var app = express();
-// app.use('/graphql', graphqlHTTP({
-//     schema: schema,
-//     rootValue: root,
-//     graphiql: true,
-// }));
-// app.listen(4000, () => console.log('Now browse to localhost:4000/graphql'));
+// graphql(schema, '{ hello }', root).then((response) => {
+//     console.log(response);
+// });
 
-// var express = require('express');
-// var bodyParser = require('body-parser');
-// var { graphqlExpress, graphiqlExpress } = require('graphql-server-express');
-// var { makeExecutableSchema } = require('graphql-tools');
+var express = require('express');
+var graphqlHTTP = require('express-graphql');
+var { buildSchema } = require('graphql');
 
-// var typeDefs = [`
-// type Query {
-//   hello: String
-// }
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
 
-// schema {
-//   query: Query
-// }`];
+var root = { hello: () => 'Hello world!' };
 
-// var resolvers = {
-//     Query: {
-//         hello(root) {
-//             return 'world';
-//         }
-//     }
-// };
+var app = express();
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
+app.listen(4000, () => console.log('Now browse to localhost:4000/graphql'));
 
-// var schema = makeExecutableSchema({ typeDefs, resolvers });
-// var app = express();
-// app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
-// app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
-// app.listen(4000, () => console.log('Now browse to localhost:4000/graphiql'));
+var express = require('express');
+var bodyParser = require('body-parser');
+var { graphqlExpress, graphiqlExpress } = require('graphql-server-express');
+var { makeExecutableSchema } = require('graphql-tools');
+
+var typeDefs = [`
+type Query {
+  hello: String
+}
+
+schema {
+  query: Query
+}`];
+
+var resolvers = {
+    Query: {
+        hello(root) {
+            return 'world';
+        }
+    }
+};
+
+var schema = makeExecutableSchema({ typeDefs, resolvers });
+var app = express();
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+app.listen(4000, () => console.log('Now browse to localhost:4000/graphiql'));
